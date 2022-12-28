22e4ab8ee0bc00eafb02b8e27d8f6a576f24fb9b
Muhammad Shahbaz <72975073+mshahbazco786@users.noreply.github.com>
2022-12-28 10:28:36 +0500

    Update add_asyn_to_script.php

--- add_asyn_to_script.php
+++ add_asyn_to_script.php
@@ -15,7 +15,7 @@
 add_filter( 'script_loader_tag', 'add_async_to_script', 10, 3 );
 function add_async_to_script( $tag, $handle, $src ) {
 	//You can use this to make all async
-	$tag = str_replace( ' src', ' defer src', $tag );
+	$tag = str_replace( ' src', ' async src', $tag );
     
     /*
         You can use this to make it work as below for a specific script
@@ -26,7 +26,7 @@ function add_async_to_script( $tag, $handle, $src ) {
     */
 
     // For All script Tags
-    $tag = '<script defer type="text/javascript" src="' . esc_url( $src ) . '"></script>';
+    $tag = '<script async type="text/javascript" src="' . esc_url( $src ) . '"></script>';
 
 	return $tag;
 }
